828ae0f58570c698b9b28764950f0f31806c81e6
Laurent <laurentiuslionel@gmail.com>
2025-06-05 12:25:15 +0700

    feat: add food images to main layout

--- app/page.js
+++ app/page.js
@@ -1,20 +1,35 @@
+import Image from "next/image";
+import tahuIsi from "../public/tahu-isi.jpg";
+import keripikKaca from "../public/keripik-kaca.jpeg";
+import basreng from "../public/basreng.jpeg";
+
 export default function Home () {
     return (
-        <div className="grid place-items-center w-screen px-16 py-32 gap-25 grid-rows-[auto_auto_auto]">
+        <div className="grid place-items-center w-screen px-16 py-32 gap-10 grid-rows-[auto_auto_auto]">
             <div className="flex flex-col items-center gap-2">
                 <h1 className="text-xl font-sans text-center font-bold">Lorem ipsum dolor sit amet.</h1>
-                <p className="text-[#bbb] font-mono text-center">Consetectur adispicing elit.</p>
-                <button className="border-1 font-sans bg-[#fff] transition-all duration-300 ease-out hover:bg-[#dfdfdf] mt-4 
-                text-[#0a0a0a] rounded-full px-6 py-3 text-sm flex justify-center">
-                Pre-order
-                </button>
+                <h2 className="text-[#bbb] font-mono text-center">Consetectur adispicing elit.</h2>
+                <button className="border-1 font-sans bg-[#fff] transition-all duration-300 ease-out hover:bg-[#dfdfdf] 
+                mt-4 text-[#0a0a0a] rounded-full px-6 py-3 text-sm flex justify-center">Pre-order</button>
+            </div>
+            <div className="flex flex-col items-center mt-10 font-sans gap-5">
+                <h2 className="font-sans font-bold text-lg mb-1">Food</h2>
+                <div className="flex flex-col items-center gap-1">
+                    <Image className="rounded-lg shadow-md" src={tahuIsi} alt="Tahu Isi" width={200} height={120}></Image>
+                    <h3>Tahu Isi</h3>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                    <Image className="rounded-lg shadow-md" src={keripikKaca} alt="Keripik Kaca" width={200} height={120}></Image>
+                    <h3>Keripik Kaca</h3>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                    <Image className="rounded-lg shadow-md" src={basreng} alt="Basreng" width={200} height={120}></Image>
+                    <h3>Basreng</h3>
+                </div>
             </div>
             <div className="flex flex-col items-center">
-                <h1 className="font-sans font-bold text-lg">Food</h1>
-            </div>
-            <div className="flex flex-col items-center">
-                <h1 className="font-sans font-bold text-lg">Drink</h1>
+                <h2 className="font-sans font-bold text-lg">Drink</h2>
             </div>
         </div>
-    )
+    );
 }
