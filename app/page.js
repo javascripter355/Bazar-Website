b0d5f750abceb51a5eeae98da4a7c955ba0ea60f
Laurent <laurentiuslionel@gmail.com>
2025-06-08 07:56:13 +0700

    feat: add blur and reduce opacity when pre-order button is clicked

--- app/page.js
+++ app/page.js
@@ -11,6 +11,8 @@ import airMineral from "../public/air-mineral.webp";
 import { useTheme } from "next-themes";
 
 export default function Home () {
+    const [popUp, setPopUp] = useState(false);
+
     const variants = {
         hidden: {
             y: 10,
@@ -31,14 +33,14 @@ export default function Home () {
     const { setTheme } = useTheme()
 
     return (
-        <div className="grid place-items-center w-screen px-16 py-32 gap-16 grid-rows-[auto_auto_auto]">
+        <div className={`grid place-items-center w-screen px-16 py-32 gap-16 ${ popUp ? 'blur-[3px] opacity-60' : '' } grid-rows-[auto_auto_auto] transition-all duration-500`}>
             <motion.div variants={variants} initial="hidden" whileInView="show" viewport={{ margin: "-50px", once: true }} className="flex flex-col items-center gap-2">
                 <motion.h1 variants={variants} className="text-3xl font-sans text-center font-bold">Lorem ipsum <span className="bg-gradient-to-r from-blue-300 to-[#ddd] bg-clip-text text-transparent">dolor</span> sit amet.</motion.h1>
                 <motion.h2 variants={variants} className="text-[#bbb] font-mono text-center">Consetectur adispicing elit.</motion.h2>
                 <motion.span variants={variants} className="hidden"></motion.span>
                 <motion.span variants={variants} className="hidden"></motion.span>
                 <motion.div variants={variants}>
-                    <Button className="font-sans mt-4">Pre-order</Button></motion.div>
+                    <Button className="font-sans mt-4" onClick={() => setPopUp(true)}>Pre-order</Button></motion.div>
             </motion.div>
             <motion.div variants={variants} className="flex flex-col items-center mt-10 font-sans gap-5">
                 <motion.h2 variants={variants} initial="hidden" whileInView="show" viewport={{ margin: "-50px", once: true }} className="font-sans font-bold text-2xl mb-1">Food</motion.h2>
